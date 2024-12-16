71a48d978ce5a0e626654051be4b64b81a113c00
Kauany <raquelkauany17@gmail.com>
2024-12-16 20:20:13 -0300

    display de datas dinâmicas finalizado

--- script.js
+++ script.js
@@ -13,6 +13,20 @@ seletordata.addEventListener("click", () => {
     calendario.hidden = false;
 });
 
+const clickData = (e) => {
+    const button = e.target;
+
+    // remove a classe "selecionado" dos outros botões
+    const selecionado = datas.querySelector(".selecionado");
+    selecionado && selecionado.classList.remove("selecionado");
+
+    //adiciona a classe "selecionado" para o botão atual
+    button.classList.add("selecionado");
+
+    // define a data selecionada 
+    dataSelecionada = new Date(ano, mes, parseInt(button.textContent));
+};
+
 //esconder calendário
 cancelarb.addEventListener("click", () => {
     calendario.hidden = true; 
@@ -35,6 +49,7 @@ datas.innerHTML = "";
 const ultimaDoMesAnterior = new Date(ano, mes, 0 );
 
 for (let i = 0; i <= ultimaDoMesAnterior.getDay(); i++) {
+
     const text = ultimaDoMesAnterior.getDate() - ultimaDoMesAnterior.getDay() + i;
     const button = createButton(text, true, false);
     datas.appendChild(button);
@@ -45,19 +60,31 @@ for (let i = 0; i <= ultimaDoMesAnterior.getDay(); i++) {
 const ultimoDoMes = new Date(ano, mes +1, 0);
 
 for(let i = 1; i <= ultimoDoMes.getDate(); i++) {
-    const button = createButton (i, false, false);
+
+    const eHoje = dataSelecionada.getDate() === i && dataSelecionada.getFullYear() === ano && dataSelecionada.getMonth() === mes;
+
+    const button = createButton (i, false, eHoje);
+    button.addEventListener("click", clickData);
     datas.appendChild(button);
 }
 
 //mostrar a primeira semana do mês seguinte
 
+const primeiraDoProximomes = new Date(ano, mes, + 1, 1);
+
+for (let i = primeiraDoProximomes.getDay(); i < 4; i++) {
+    const text = primeiraDoProximomes.getDate() - primeiraDoProximomes.getDay() + i;
+    const button = createButton(text, true, false);
+    datas.appendChild(button);
+}
+
 };
 
-const createButton = (text, isDisabled = false, isToday = false) => {
+const createButton = (text, isDisabled = false, eHoje = false) => {
     const button = document.createElement("button");
     button.textContent = text;
     button.disabled = isDisabled;
-    button.classList.toggle("today", isToday);
+    button.classList.toggle("hoje", eHoje);
     return button;
 }
 
